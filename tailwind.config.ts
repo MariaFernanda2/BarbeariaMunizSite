e9b0370afdc20196e83105453c71f3ce450b42e5
MariaFernanda2 <mariafernanda.samuniz@gmail.com>
2024-01-30 18:16:05 -0300

    barbershop-item, footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   prefix: "",
   theme: {
     container: {
@@ -75,14 +75,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} satisfies Config
 
-module.exports = {
-  // ... outras configurações do Tailwind ...
-  prettier: {
-    plugins: ['tailwindcss'],
-  },
-};
-
-
-export default config;
+export default config
