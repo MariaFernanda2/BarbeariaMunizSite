f2af5761d0bad3e66a1f60978a3a9d0558517fd9
MariaFernanda2 <mariafernanda.samuniz@gmail.com>
2026-03-23 21:20:39 -0300

    fix: liberando dominio da nova imagem da barbearia

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "utfs.io",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "cl1ro7kgga.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
