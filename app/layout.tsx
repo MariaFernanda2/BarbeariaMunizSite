c6e7b5aed89dd655afbdd7cbc9e538a612a4016d
MariaFernanda2 <mariafernanda.samuniz@gmail.com>
2026-04-10 20:35:43 -0300

    feat(metadata): adiciona configurações de SEO e compartilhamento do site

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,22 +10,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Barbearia Muniz",
   description: "Aplicação para agendamentos da barbearia",
+  openGraph: {
+    title: "Barbearia Muniz",
+    description: "Agende seu horário na Barbearia Muniz ✂️",
+    url: "https://www.barbeariamuniz.com.br",
+    siteName: "Barbearia Muniz",
+    images: [
+      {
+        url: "https://cl1ro7kgga.ufs.sh/f/vKcUisEYIxmgkAK8AG54z9SB3LdMP6CNOGeX78fyWu0KagEw",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} dark`}>
-        <AuthProvider>
-          <div className="flex-1">{children}</div>
-          <Toaster />
-          <Footer />
-        </AuthProvider>
-      </body>
-    </html>
-  );
-}
